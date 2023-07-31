c364c6efd01c2f5345ee4c4863cdd22e687b0e25
mehmetresitcagan@posta.mu.edu.tr <mehmetresitcagan@posta.mu.edu.tr>
2023-07-31 08:39:44 +0300

    initial commit

---
+++ Deneme/Deneme/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace Deneme {
+    class Program {
+        static  void Main(string[] args) {
+            byte number = 5;
+            float a = 5.6f;
+            Console.WriteLine(a+number);
+        }
+    }
+}
---
+++ dotnet-deneme/dotnet-deneme/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+int b = 3;
+byte i = (byte)b;
+Console.WriteLine(b);
